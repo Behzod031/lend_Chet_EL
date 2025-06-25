83bb6f908f1472ab10d7ca7ad845a0d67185e087
Behzod031 <behzod031@gmail.com>
2025-06-26 00:49:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
-from flask import Flask, render_template, redirect, request
+import os
+from flask import Flask, render_template, request
 import gspread
 from google.oauth2.service_account import Credentials
 
 app = Flask(__name__)
 
-# Путь к JSON-файлу с ключами
-SERVICE_ACCOUNT_FILE = r"C:\Users\XS-NB-OP\PycharmProjects\call_center - marketing_bot\credentials.json"
+# Путь к JSON-файлу с ключами: из переменной окружения или по умолчанию рядом с app.py
+SERVICE_ACCOUNT_FILE = os.environ.get(
+    "GOOGLE_CREDENTIALS_PATH",
+    os.path.join(os.path.dirname(__file__), "credentials.json")
+)
 
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
 
+# Авторизация в Google Sheets
 creds = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 client = gspread.authorize(creds)
 
-# Открываем таблицу
+# Открываем таблицу и лист
 spreadsheet = client.open("Chet-el mijozlar")
 worksheet = spreadsheet.worksheet("MyLandingDB")
 
@@ -26,13 +31,12 @@ def index():
         phone = request.form.get('phone')
         country = request.form.get('country')
 
-        # Записываем в таблицу
+        # Записываем данные в таблицу
         worksheet.append_row([name, phone, country])
 
-        # После отправки формы сразу рендерим telegram.html
+        # Рендерим страницу с переходом в Telegram
         return render_template('telegram.html')
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=1232)
-#
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 1232)), debug=False)
